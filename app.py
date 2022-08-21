f5ba5fa8e06dff94914ae2a5e9ee8fce0e1fb2ca
gabrielmj23 <gabrielmj2312@gmail.com>
2022-08-20 22:08:05 -0400

    Add connection to database

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Set up environment variables
+from dotenv import load_dotenv
+load_dotenv()
 
 # Configure application
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# Connect app to database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.getenv('SECRET_KEY')
 
 # Stop responses from being cached
 @app.after_request
@@ -11,5 +23,3 @@ def after_request(response):
   response.headers['Expires'] = 0
   response.headers['Pragma'] = 'no-cache'
   return response
-
-  
