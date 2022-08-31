bc7812f4f8271c9e39db5b09ee96c20f27b56b15
gabrielmj23 <gabrielmj2312@gmail.com>
2022-08-31 16:11:57 -0400

    Add port to DB connection and favicon to site

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ dbConnection = psycopg2.connect(
   dbname = 'cherrypics',
   user = os.getenv('DB_USER'),
   password = os.getenv('DB_PASSWORD'),
-  host = os.getenv('DB_HOST')
+  host = os.getenv('DB_HOST'),
+  port = os.getenv('DB_PORT')
 )
 db = dbConnection.cursor()
 
